e698a51349643d2f9219c383b9042157205db987
ellmetha <morgan.aubert@zoho.com>
2013-12-26 18:43:38 +0100

    Some translations added for the abstract dated model

--- machina/models/abstract_models.py
+++ machina/models/abstract_models.py
@@ -3,6 +3,7 @@
 # Standard library imports
 # Third party imports
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Local application / specific library imports
 
@@ -33,8 +34,8 @@ class DatedModel(models.Model):
     """
     An abstract base class model that provides a created and a updated fields to store creation date and last updated date.
     """
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('Creation date'))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_('Update date'))
 
     class Meta:
         abstract = True
