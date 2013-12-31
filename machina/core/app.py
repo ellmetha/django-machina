565c98a7df9af3fc2873bbe0572a3bef1d328cc4
ellmetha <morgan.aubert@zoho.com>
2013-12-31 16:48:27 +0100

    'App' wrapper for views and URLs started

---
+++ machina/core/app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+# Third party imports
+# Local application / specific library imports
+
+
+class Application(object):
+    """
+    Machina's views and URLs use a tree of 'app' instances, each of which subclass this Application
+    class and provide an 'urls' property.
+    This process is directly inspired from the one provided by django-oscar.
+    """
+    name = None
+
+    def __init__(self, app_name=None, **kwargs):
+        self.app_name = app_name
+        # Set all kwargs as object attributes
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+
+    def get_urls(self):
+        """
+        Return the url patterns for the considered app. It must be implemented in
+        any subclass.
+        """
+        raise NotImplementedError
+
+    @property
+    def urls(self):
+        # We set the application and instance namespace here
+        return self.get_urls(), self.app_name, self.name
