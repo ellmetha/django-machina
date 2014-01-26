afb3a2fcb9167038fe1843ee27d63882e377e3d6
ellmetha <morgan.aubert@zoho.com>
2014-01-26 23:51:34 +0100

    Permission handler unit tests added

--- tests/unit/permission/test_handler.py
+++ tests/unit/permission/test_handler.py
@@ -2,11 +2,15 @@
 
 # Standard library imports
 # Third party imports
+from django.contrib.auth.models import Group
 from django.contrib.auth.models import User
 from django.db.models import get_model
 from guardian.shortcuts import assign_perm
+from guardian.shortcuts import remove_perm
 
 # Local application / specific library imports
+from machina.apps.conversation.abstract_models import TOPIC_STATUSES
+from machina.apps.conversation.abstract_models import TOPIC_TYPES
 from machina.apps.forum.abstract_models import FORUM_TYPES
 from machina.core.loading import get_class
 from machina.test.testcases import BaseUnitTestCase
@@ -20,6 +24,8 @@ PermissionHandler = get_class('permission.handler', 'PermissionHandler')
 class TestPermissionHandler(BaseUnitTestCase):
     def setUp(self):
         self.u1 = User.objects.create(username='user1')
+        self.g1 = Group.objects.create(name='group1')
+        self.u1.groups.add(self.g1)
 
         # Permission handler
         self.perm_handler = PermissionHandler()
@@ -37,15 +43,65 @@ class TestPermissionHandler(BaseUnitTestCase):
         top_level_link = Forum.objects.create(name='top_level_link', type=FORUM_TYPES.forum_link)
         self.top_level_link = top_level_link
 
+        # Set up some topics
+        self.forum_1_topic = Topic.objects.create(subject='Test topic 1', forum=self.forum_1, poster=self.u1,
+                                                  type=TOPIC_TYPES.topic_post, status=TOPIC_STATUSES.topic_unlocked)
+        self.forum_3_topic = Topic.objects.create(subject='Test topic 2', forum=self.forum_3, poster=self.u1,
+                                                  type=TOPIC_TYPES.topic_post, status=TOPIC_STATUSES.topic_unlocked)
+
+        # Set up some posts
+        self.post_1 = Post.objects.create(topic=self.forum_1_topic, poster=self.u1, content='hello 1')
+        self.post_2 = Post.objects.create(topic=self.forum_3_topic, poster=self.u1, content='hello 2')
+
         # Assign some permissions
         assign_perm('can_see_forum', self.u1, self.top_level_cat)
         assign_perm('can_see_forum', self.u1, self.forum_1)
-        assign_perm('can_read_forum', self.u1, self.forum_3)
+        assign_perm('can_read_forum', self.g1, self.forum_3)
 
-    def test_can_filter_a_list_of_forum_against_a_user(self):
+    def test_show_a_forum_if_it_is_visible(self):
+        # Setup
+        forums = Forum.objects.filter(pk=self.top_level_cat.pk)
+        # Run
+        filtered_forums = self.perm_handler.forum_list_filter(forums, self.u1)
+        # Check
+        self.assertQuerysetEqual(filtered_forums, [self.top_level_cat, ])
+
+    def test_hide_a_forum_if_it_is_not_visible(self):
+        # Setup
+        forums = Forum.objects.filter(pk=self.top_level_cat.pk)
+        # Run
+        filtered_forums = self.perm_handler.forum_list_filter(forums, self.u1)
+        # Check
+        self.assertQuerysetEqual(filtered_forums, [self.top_level_cat, ])
+
+    def test_show_a_forum_if_all_of_its_ancestors_are_visible(self):
         # Setup
         forums = Forum.objects.filter(parent=self.top_level_cat)
         # Run
         filtered_forums = self.perm_handler.forum_list_filter(forums, self.u1)
         # Check
         self.assertQuerysetEqual(filtered_forums, [self.forum_1, self.forum_3])
+
+    def test_hide_a_forum_if_one_of_its_ancestors_is_not_visible(self):
+        # Setup
+        remove_perm('can_see_forum', self.u1, self.top_level_cat)
+        forums = Forum.objects.filter(parent=self.top_level_cat)
+        # Run
+        filtered_forums = self.perm_handler.forum_list_filter(forums, self.u1)
+        # Check
+        self.assertQuerysetEqual(filtered_forums, [])
+
+    def test_knows_the_last_topic_visible_inside_a_forum(self):
+        # Run & check : no forum hidden
+        last_post = self.perm_handler.get_forum_last_post(self.top_level_cat, self.u1)
+        self.assertEqual(last_post, self.post_2)
+
+        # Run & check : one forum hidden
+        remove_perm('can_read_forum', self.g1, self.forum_3)
+        last_post = self.perm_handler.get_forum_last_post(self.top_level_cat, self.u1)
+        self.assertEqual(last_post, self.post_1)
+
+        # Run & check : all forums hidden
+        remove_perm('can_see_forum', self.u1, self.forum_1)
+        last_post = self.perm_handler.get_forum_last_post(self.top_level_cat, self.u1)
+        self.assertIsNone(last_post)
