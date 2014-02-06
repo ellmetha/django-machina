116bdac1e97704bf7f849047bc1de8f18353cc69
ellmetha <morgan.aubert@zoho.com>
2014-02-06 01:04:12 +0100

    Fourm tags tests updated

--- tests/integration/templatetags/test_forum_tags.py
+++ tests/integration/templatetags/test_forum_tags.py
@@ -6,7 +6,9 @@ from django.contrib.auth.models import User
 from django.db.models import get_model
 from django.template import Context
 from django.template.base import Template
+from django.template.loader import render_to_string
 from django.test import TestCase
+from django.test.client import RequestFactory
 from guardian.shortcuts import assign_perm
 
 # Local application / specific library imports
@@ -40,7 +42,10 @@ class TestForumLastPostTag(TestCase):
         # Set up some topics and posts
         self.forum_1_topic = Topic.objects.create(subject='Test topic 1', forum=self.forum_1, poster=self.user,
                                                   type=TOPIC_TYPES.topic_post, status=TOPIC_STATUSES.topic_unlocked)
+        self.forum_2_topic = Topic.objects.create(subject='Test topic 2', forum=self.forum_2, poster=self.user,
+                                                  type=TOPIC_TYPES.topic_post, status=TOPIC_STATUSES.topic_unlocked)
         self.post_1 = Post.objects.create(topic=self.forum_1_topic, poster=self.user, content='hello 1')
+        self.post_2 = Post.objects.create(topic=self.forum_2_topic, poster=self.user, content='hello 2')
 
         # Assign some permissions
         assign_perm('can_see_forum', self.user, self.top_level_cat)
@@ -55,3 +60,41 @@ class TestForumLastPostTag(TestCase):
         # Check
         self.assertEqual(rendered, '')
         self.assertEqual(c['var'], self.post_1)
+
+
+class TestForumListTag(TestCase):
+    def setUp(self):
+        self.loadstatement = '{% load forum_tags %}'
+        self.request_factory = RequestFactory()
+        self.user = User.objects.create(username='testuser')
+
+        # Set up a top-level category
+        top_level_cat = Forum.objects.create(name='top_level_cat', type=FORUM_TYPES.forum_cat)
+        self.top_level_cat = top_level_cat
+
+        # Set up some forums
+        self.forum_1 = Forum.objects.create(parent=top_level_cat, name='forum_1', type=FORUM_TYPES.forum_post)
+        self.forum_2 = Forum.objects.create(parent=top_level_cat, name='forum_2', type=FORUM_TYPES.forum_post)
+
+    def test_can_render_a_list_of_forums_according_to_their_minimum_tree_level(self):
+        # Setup
+        forums = Forum.objects.all()
+        request = self.request_factory.get('/')
+        request.user = self.user
+        t = Template(self.loadstatement + '{% forum_list forums request %}')
+        c = Context({'forums': forums, 'request': request})
+        expected_out = render_to_string(
+            'machina/forum/forum_list.html',
+            {
+                'forums': forums,
+                'user': self.user,
+                'root_level': 0,
+                'root_level_middle': 1,
+                'root_level_sub': 2,
+            }
+        )
+        # Run
+        rendered = t.render(c)
+        # Check
+        self.assertNotEqual(rendered, '')
+        self.assertEqual(rendered, expected_out)
