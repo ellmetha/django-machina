52129a4c7a623c1f53cb021b3aa44bc25593d3c3
ellmetha <morgan.aubert@zoho.com>
2014-03-02 22:37:59 +0100

    'tracking' template tags updated and get_unread_topics templatetag added

--- machina/templatetags/tracking_tags.py
+++ machina/templatetags/tracking_tags.py
@@ -5,16 +5,12 @@ from __future__ import unicode_literals
 
 # Third party imports
 from django import template
-from django.db.models import get_model
 
 # Local application / specific library imports
 from machina.core.loading import get_class
 
-Forum = get_model('forum', 'Forum')
-ForumReadTrack = get_model('tracking', 'ForumReadTrack')
-
-PermissionHandler = get_class('permission.handler', 'PermissionHandler')
-perm_handler = PermissionHandler()
+TrackingHandler = get_class('tracking.handler', 'TrackingHandler')
+tracks_handler = TrackingHandler()
 
 register = template.Library()
 
@@ -28,30 +24,16 @@ def get_unread_forums(forums, user):
 
         {% get_unread_forums forums request.user as unread_forums %}
     """
-    unread_forums = []
-    processed_forums = []
+    return tracks_handler.get_unread_forums(forums, user)
 
-    # A user which is not authenticated will never see a forum as unread
-    if not user.is_authenticated():
-        return unread_forums
 
-    # Forums are sorted by their position in the forums tree in order
-    # to limit the number of operations required to determine the unread
-    # forums below a forum which has the smallest position among the
-    # forums from the initial set.
-    # In fact, a forum is unread if at least one of its descendants
-    # (including itself) is unread.
-    # So the forums are sorted and then are processed by batches: from
-    # a forum with a small level and all of its descendants, we get the
-    # unread forums, and so on.
-    sorted_forums = sorted(forums, key=lambda forum: forum.level)
+@register.assignment_tag
+def get_unread_topics(topics, user):
+    """
+    This will return a list of unread topics for the given user from a given set of forums.
 
-    for forum in sorted_forums:
-        if forum not in unread_forums and forum not in processed_forums:
-            readable_forums = perm_handler.forum_list_filter(
-                forum.get_descendants(include_self=True), user)
-            unread = ForumReadTrack.objects.get_unread_forums_from_list(readable_forums, user)
-            unread_forums.extend(unread)
-            processed_forums.extend(readable_forums)
+    Usage::
 
-    return unread_forums
+        {% get_unread_topics topics request.user as unread_topics %}
+    """
+    return tracks_handler.get_unread_topics(topics, user)
