4061e635e32733439ead7eb8e8d131737ac233df
ellmetha <morgan.aubert@zoho.com>
2014-07-02 01:16:05 +0200

    Example project updated

--- example_projects/vanilla/src/example_project/settings/base.py
+++ example_projects/vanilla/src/example_project/settings/base.py
@@ -11,6 +11,8 @@ TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
