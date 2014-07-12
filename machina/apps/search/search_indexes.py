0e17f2900034da993c680862e7cfe6bc6b2aa809
ellmetha <morgan.aubert@zoho.com>
2014-07-12 20:28:09 +0200

    Post search index updated

--- machina/apps/search/search_indexes.py
+++ machina/apps/search/search_indexes.py
@@ -11,11 +11,26 @@ Post = get_model('conversation', 'Post')
 
 
 class PostIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.EdgeNgramField(document=True, use_template=True, template_name='search/post_text.txt')
+    text = indexes.CharField(document=True, use_template=True, template_name='search/post_text.txt')
     poster = indexes.CharField(model_attr='poster')
+
+    forum = indexes.IntegerField(model_attr='topic__forum_id')
+    forum_name = indexes.CharField()
+
+    topic = indexes.IntegerField(model_attr='topic_id')
+    topic_subject = indexes.CharField()
 
     created = indexes.DateTimeField(model_attr='created')
     updated = indexes.DateTimeField(model_attr='updated')
 
     def get_model(self):
         return Post
+
+    def prepare_forum_name(self, obj):
+        return obj.topic.forum.name
+
+    def prepare_topic_subject(self, obj):
+        return obj.topic.subject
+
+    def read_queryset(self, using=None):
+        return Post.objects.all().select_related('topic', 'poster')
