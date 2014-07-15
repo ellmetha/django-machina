b2db8983acfd99765fd6b98ae1237dc84b8d0091
ellmetha <morgan.aubert@zoho.com>
2014-07-16 00:23:47 +0200

    Unit tests added

--- tests/unit/core/test_loading.py
+++ tests/unit/core/test_loading.py
@@ -3,6 +3,7 @@
 # Standard library imports
 # Third party imports
 from django import VERSION
+from django.conf import settings
 from django.test import TestCase
 from django.test.utils import override_settings
 
@@ -43,3 +44,31 @@ class TestClassLoadingFunctions(TestCase):
             with override_settings(INSTALLED_APPS=('it.is.bad', )):
                 with self.assertRaises(AppNotFoundError):
                     get_class('bad', 'Foo')
+
+
+class TestClassLoadingFunctionsWithOverrides(TestCase):
+    def setUp(self):
+        self.installed_apps = list(settings.INSTALLED_APPS)
+        self.installed_apps[self.installed_apps.index('machina.apps.forum')] = 'tests._testsite.apps.forum'
+
+    def test_can_load_a_class_defined_in_a_local_module(self):
+        with override_settings(INSTALLED_APPS=self.installed_apps):
+            MyNewForumView = get_class('forum.views', 'MyNewForumView')
+            self.assertEqual('tests._testsite.apps.forum.views', MyNewForumView.__module__)
+
+    def test_can_load_a_class_that_is_not_defined_in_the_local_module(self):
+        with override_settings(INSTALLED_APPS=self.installed_apps):
+            ForumView = get_class('forum.views', 'ForumView')
+            self.assertEqual('machina.apps.forum.views', ForumView.__module__)
+
+    def test_can_load_a_class_from_an_app_module_that_is_not_present_in_the_local_app_module(self):
+        with override_settings(INSTALLED_APPS=self.installed_apps):
+            ForumAdmin = get_class('forum.admin', 'ForumAdmin')
+            self.assertEqual('machina.apps.forum.admin', ForumAdmin.__module__)
+
+    def test_can_load_classes_from_both_the_local_app_and_the_vanilla_app(self):
+        with override_settings(INSTALLED_APPS=self.installed_apps):
+            ForumView, MyNewForumView = get_classes(
+                'forum.views', ('ForumView', 'MyNewForumView'))
+            self.assertEqual('machina.apps.forum.views', ForumView.__module__)
+            self.assertEqual('tests._testsite.apps.forum.views', MyNewForumView.__module__)
