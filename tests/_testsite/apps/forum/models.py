4561cffbe7ba00564c254511b8df2c0803a2bc29
ellmetha <morgan.aubert@zoho.com>
2014-07-15 23:57:32 +0200

    Tests stuf updated

---
+++ tests/_testsite/apps/forum/models.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+# Third party imports
+# Local application / specific library imports
---
+++ tests/_testsite/apps/forum/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+# Third party imports
+from django.views.generic import View
+
+# Local application / specific library imports
+
+
+class MyNewForumView(View):
+    pass
