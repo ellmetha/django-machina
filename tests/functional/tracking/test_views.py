6ce9eff2c24e842636fabe72925a12a3b68c8dcd
ellmetha <morgan.aubert@zoho.com>
2014-08-26 23:57:00 +0200

    Functional tests updated

--- tests/functional/tracking/test_views.py
+++ tests/functional/tracking/test_views.py
@@ -181,3 +181,55 @@ class TestMarkTopicsReadView(BaseClientTestCase):
         response = self.client.get(correct_url, follow=True)
         # Check
         self.assertIsForbidden(response)
+
+
+class TestUnreadTopicsView(BaseClientTestCase):
+    def setUp(self):
+        super(TestUnreadTopicsView, self).setUp()
+
+        # Add some users
+        self.u1 = UserFactory.create()
+        self.u2 = UserFactory.create()
+        self.g1 = GroupFactory.create()
+        self.u1.groups.add(self.g1)
+        self.u2.groups.add(self.g1)
+        self.user.groups.add(self.g1)
+
+        # Permission handler
+        self.perm_handler = PermissionHandler()
+
+        # Tracking handler
+        self.tracks_handler = TrackingHandler()
+
+        self.top_level_cat_1 = create_category_forum()
+        self.top_level_cat_2 = create_category_forum()
+
+        self.forum_1 = create_forum(parent=self.top_level_cat_1)
+        self.forum_2 = create_forum(parent=self.top_level_cat_1)
+        self.forum_2_child_1 = create_forum(parent=self.forum_2)
+        self.forum_3 = create_forum(parent=self.top_level_cat_1)
+
+        self.forum_4 = create_forum(parent=self.top_level_cat_2)
+
+        self.topic = create_topic(forum=self.forum_2, poster=self.u1)
+        PostFactory.create(topic=self.topic, poster=self.u1)
+
+        # Initially u2 and user read the previously created topic
+        ForumReadTrackFactory.create(forum=self.forum_2, user=self.u2)
+        ForumReadTrackFactory.create(forum=self.forum_2, user=self.user)
+
+        # Assign some permissions
+        assign_perm('can_read_forum', self.g1, self.top_level_cat_1)
+        assign_perm('can_read_forum', self.g1, self.top_level_cat_2)
+        assign_perm('can_read_forum', self.g1, self.forum_1)
+        assign_perm('can_read_forum', self.g1, self.forum_2)
+        assign_perm('can_read_forum', self.g1, self.forum_2_child_1)
+        assign_perm('can_read_forum', self.g1, self.forum_4)
+
+    def test_browsing_works(self):
+        # Setup
+        correct_url = reverse('tracking:unread-topics')
+        # Run
+        response = self.client.get(correct_url, follow=True)
+        # Check
+        self.assertIsOk(response)
