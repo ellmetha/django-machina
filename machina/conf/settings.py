85a69033376bc6ba23ecc91c3f9a90119d752cbd
ellmetha <morgan.aubert@zoho.com>
2014-09-06 23:41:36 +0200

    Machina settings updated

--- machina/conf/settings.py
+++ machina/conf/settings.py
@@ -10,7 +10,7 @@ from django.conf import settings
 
 
 # General
-MACHINA_FORUM_NAME = 'Machina'
+MACHINA_FORUM_NAME = getattr(settings, 'MACHINA_FORUM_NAME', 'Machina')
 MACHINA_MARKUP_LANGUAGE = getattr(settings, 'MACHINA_MARKUP_LANGUAGE', ('precise_bbcode.utils.bbcode.render_bbcodes', {}))
 MACHINA_MARKUP_WIDGET = getattr(settings, 'MACHINA_MARKUP_WIDGET', None)
 MACHINA_BASE_TEMPLATE = getattr(settings, 'MACHINA_BASE_TEMPLATE', 'base.html')
