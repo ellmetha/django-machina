9ccd4334391343b3d710ae2ba9ffac61d755b722
ellmetha <morgan.aubert@zoho.com>
2014-10-27 23:25:31 +0100

    Unnecessary code added to support both South < 1.0 and Django 1.7 removed

--- machina/core/compat.py
+++ machina/core/compat.py
@@ -33,24 +33,3 @@ except ImportError:
 # A settings that can be used in foreign key declarations to ensure backwards compatibility
 # with Django 1.4
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
-
-
-# Django 1.7 and greater come with a built-in migration system that should be used prior to South.
-# To support South migrations with older versions of Django, developers should configure their
-# SOUTH_MIGRATION_MODULES setting.
-SOUTH_ERROR_MESSAGE = """\n
-To support South migrations, you should customize the SOUTH_MIGRATION_MODULES setting as follow:
-
-from machina.core.compat import MACHINA_SOUTH_MIGRATION_MODULES
-SOUTH_MIGRATION_MODULES = MACHINA_SOUTH_MIGRATION_MODULES
-"""
-
-MACHINA_SOUTH_MIGRATION_MODULES = {
-    'conversation': 'machina.apps.conversation.south_migrations',
-    'polls': 'machina.apps.conversation.polls.south_migrations',
-    'attachments': 'machina.apps.conversation.attachments.south_migrations',
-    'forum': 'machina.apps.forum.south_migrations',
-    'member': 'machina.apps.member.south_migrations',
-    'permission': 'machina.apps.permission.south_migrations',
-    'tracking': 'machina.apps.tracking.south_migrations',
-}
