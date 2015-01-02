7b4b220510e54ef7b78246c8f444c5938f4509b1
ellmetha <morgan.aubert@zoho.com>
2015-01-02 22:08:55 +0100

    Haystack search form updated to prevent searches on non-allowed forums

--- machina/apps/search/forms.py
+++ machina/apps/search/forms.py
@@ -31,12 +31,20 @@ class SearchForm(FacetedSearchForm):
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
+
         super(SearchForm, self).__init__(*args, **kwargs)
+
         # Update some fields
         self.fields['q'].label = _('Search for keywords')
         self.fields['q'].widget.attrs['placeholder'] = _('Keywords or phrase')
         self.fields['search_poster_name'].widget.attrs['placeholder'] = _('Poster name')
-        self.fields['search_forums'].choices = [(f.id, '{} {}'.format('-' * f.margin_level, f.name)) for f in perm_handler.forum_list_filter(Forum.objects.all(), user)]
+
+        self.allowed_forums = perm_handler.forum_list_filter(Forum.objects.all(), user)
+        if self.allowed_forums:
+            self.fields['search_forums'].choices = [(f.id, '{} {}'.format('-' * f.margin_level, f.name)) for f in self.allowed_forums]
+        else:
+            # The user cannot view any single forum, the 'search_forums' field can be deleted
+            del self.fields['search_forums']
 
     def search(self):
         sqs = super(SearchForm, self).search()
@@ -52,8 +60,11 @@ class SearchForm(FacetedSearchForm):
         if self.cleaned_data['search_poster_name']:
             sqs = sqs.filter(poster_name__icontains=self.cleaned_data['search_poster_name'])
 
-        # Handles searches in specific forums
-        if self.cleaned_data['search_forums']:
+        # Handles searches in specific forums if necessary
+        if 'search_forums' in self.cleaned_data and self.cleaned_data['search_forums']:
             sqs = sqs.filter(forum__in=self.cleaned_data['search_forums'])
+        else:
+            forum_ids = self.allowed_forums.values_list('id', flat=True)
+            sqs = sqs.filter(forum__in=forum_ids) if forum_ids else sqs.none()
 
         return sqs
