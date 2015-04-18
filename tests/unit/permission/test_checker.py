4c49d214ddd3ab5f6a7f0b0270f63055081e94f9
ellmetha <morgan.aubert@zoho.com>
2015-04-18 12:54:10 +0200

    ForumPermissionCheck tests added

--- tests/unit/permission/test_checker.py
+++ tests/unit/permission/test_checker.py
@@ -19,6 +19,23 @@ class TestForumPermissionChecker(TestCase):
     def tearDown(self):
         machina_settings.DEFAULT_AUTHENTICATED_USER_FORUM_PERMISSIONS= []
 
+    def test_knows_that_a_superuser_has_all_the_permissions(self):
+        # Setup
+        user = UserFactory.create(is_active=True, is_superuser=True)
+        checker = ForumPermissionChecker(user)
+        # Run & check
+        self.assertTrue(checker.has_perm('can_see_forum', self.forum))
+        self.assertTrue(checker.has_perm('can_read_forum', self.forum))
+
+    def test_knows_that_an_inactive_user_has_no_permissions(self):
+        # Setup
+        user = UserFactory.create(is_active=False)
+        checker = ForumPermissionChecker(user)
+        # Run & check
+        self.assertFalse(checker.has_perm('can_see_forum', self.forum))
+        self.assertFalse(checker.has_perm('can_read_forum', self.forum))
+        self.assertEqual(checker.get_perms(self.forum), [])
+
     def test_allows_the_use_of_default_permissions(self):
         # Setup
         user = UserFactory.create()
