fd294a68a15f00fc491b861febf70f375a914fcb
ellmetha <morgan.aubert@zoho.com>
2015-06-23 00:59:35 +0200

    Permission handler updated

--- machina/apps/forum_permission/handler.py
+++ machina/apps/forum_permission/handler.py
@@ -198,6 +198,12 @@ class PermissionHandler(object):
         moderated_forums = self._get_forums_for_user(user, perms)
         return moderated_forums.exists()
 
+    def can_close_topics(self, forum, user):
+        """
+        Given a forum, checks whether the user can close its topics.
+        """
+        return self._perform_basic_permission_check(forum, user, 'can_close_topics')
+
     def can_move_topics(self, forum, user):
         """
         Given a forum, checks whether the user can move its topics to another forum.
