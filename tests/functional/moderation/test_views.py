2722d625f72cc7aa5cef5a0deb84f856621fadba
ellmetha <morgan.aubert@zoho.com>
2015-07-05 23:32:51 +0200

    Topic delete view tests added

--- tests/functional/moderation/test_views.py
+++ tests/functional/moderation/test_views.py
@@ -4,8 +4,10 @@
 from __future__ import unicode_literals
 
 # Third party imports
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from faker import Factory as FakerFactory
+import pytest
 
 # Local application / specific library imports
 from machina.core.loading import get_class
@@ -96,3 +98,64 @@ class TestTopicCloseView(BaseClientTestCase):
         last_url, status_code = response.redirect_chain[-1]
         assert topic_url in last_url
 
+
+class TestTopicDeleteView(BaseClientTestCase):
+    def setUp(self):
+        super(TestTopicDeleteView, self).setUp()
+
+        # Permission handler
+        self.perm_handler = PermissionHandler()
+
+        # Set up a top-level forum
+        self.top_level_forum = create_forum()
+
+        # Set up a topic and some posts
+        self.topic = create_topic(forum=self.top_level_forum, poster=self.user)
+        self.first_post = PostFactory.create(topic=self.topic, poster=self.user)
+        self.post = PostFactory.create(topic=self.topic, poster=self.user)
+
+        # Mark the forum as read
+        ForumReadTrackFactory.create(forum=self.top_level_forum, user=self.user)
+
+        # Assign some permissions
+        assign_perm('can_read_forum', self.user, self.top_level_forum)
+        assign_perm('can_reply_to_topics', self.user, self.top_level_forum)
+        assign_perm('can_edit_own_posts', self.user, self.top_level_forum)
+        assign_perm('can_delete_own_posts', self.user, self.top_level_forum)
+        assign_perm('can_delete_posts', self.user, self.top_level_forum)
+
+    def test_browsing_works(self):
+        # Setup
+        correct_url = reverse(
+            'forum-moderation:topic-delete',
+            kwargs={'slug': self.topic.slug, 'pk': self.topic.pk})
+        # Run
+        response = self.client.get(correct_url, follow=True)
+        # Check
+        self.assertIsOk(response)
+
+    def test_can_delete_topics(self):
+        # Setup
+        correct_url = reverse(
+            'forum-moderation:topic-delete',
+            kwargs={'slug': self.topic.slug, 'pk': self.topic.pk})
+        # Run
+        response = self.client.post(correct_url, follow=True)
+        # Check
+        with pytest.raises(ObjectDoesNotExist):
+            topic = Topic.objects.get(pk=self.topic.pk)
+
+    def test_redirects_to_the_forum_view(self):
+        # Setup
+        correct_url = reverse(
+            'forum-moderation:topic-delete',
+            kwargs={'slug': self.topic.slug, 'pk': self.topic.pk})
+        # Run
+        response = self.client.post(correct_url, follow=True)
+        # Check
+        forum_url = reverse(
+            'forum:forum',
+            kwargs={'slug': self.top_level_forum.slug, 'pk': self.top_level_forum.pk})
+        self.assertGreater(len(response.redirect_chain), 0)
+        last_url, status_code = response.redirect_chain[-1]
+        assert forum_url in last_url
