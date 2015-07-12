898ee10881a0003ab1170f6740097814424b9be4
ellmetha <morgan.aubert@zoho.com>
2015-07-12 17:47:53 +0200

    Form widget added

---
+++ machina/forms/widgets.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+from __future__ import unicode_literals
+
+# Third party imports
+from django.forms.widgets import Select
+from django.utils.encoding import force_unicode
+from django.utils.html import escape, conditional_escape
+
+# Local application / specific library imports
+
+
+# Originaly comes from https://djangosnippets.org/snippets/2453/
+class SelectWithDisabled(Select):
+    """
+    Subclass of Django's select widget that allows disabling options.
+    To disable an option, pass a dict instead of a string for its label,
+    of the form: {'label': 'option label', 'disabled': True}
+    """
+    def render_option(self, selected_choices, option_value, option_label):
+        option_value = force_unicode(option_value)
+        if (option_value in selected_choices):
+            selected_html = ' selected="selected"'
+        else:
+            selected_html = ''
+        disabled_html = ''
+        if isinstance(option_label, dict):
+            if dict.get(option_label, 'disabled'):
+                disabled_html = ' disabled="disabled"'
+            option_label = option_label['label']
+        return '<option value="%s"%s%s>%s</option>' % (
+            escape(option_value), selected_html, disabled_html,
+            conditional_escape(force_unicode(option_label)))
