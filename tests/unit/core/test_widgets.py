3cca11f6c0fb29edbbdcde2af5d9b43d606d87d7
ellmetha <morgan.aubert@zoho.com>
2015-07-13 00:05:00 +0200

    Form widget tests added

---
+++ tests/unit/core/test_widgets.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+from __future__ import unicode_literals
+
+# Third party imports
+from faker import Factory as FakerFactory
+
+# Local application / specific library imports
+from machina.forms.widgets import SelectWithDisabled
+
+faker = FakerFactory.create()
+
+
+class TestSelectWithDisabled(object):
+    def test_can_render_a_single_option(self):
+        # Setup
+        widget = SelectWithDisabled()
+        # Run
+        rendered = widget.render_option([], '1', 'Test forum')
+        # Check
+        assert rendered == '<option value="1">Test forum</option>'
+
+    def test_can_render_a_single_option_that_is_selected(self):
+        # Setup
+        widget = SelectWithDisabled()
+        # Run
+        rendered = widget.render_option(['1', ], '1', 'Test forum')
+        # Check
+        assert rendered == '<option value="1" selected="selected">Test forum</option>'
+
+    def test_can_render_a_single_disabled_option(self):
+        # Setup
+        widget = SelectWithDisabled()
+        # Run
+        rendered = widget.render_option([], '1', {'label': 'Test forum', 'disabled': True})
+        # Check
+        assert rendered == '<option value="1" disabled="disabled">Test forum</option>'
