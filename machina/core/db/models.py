a74554599558e9ca1fdc7288d0d9b46c36565f0d
ellmetha <morgan.aubert@zoho.com>
2015-11-20 00:39:35 +0100

    core models module updated

--- machina/core/db/models.py
+++ machina/core/db/models.py
@@ -38,34 +38,25 @@ else:
 
     def get_model(app_label, model_name):
         """
-        Fetches a Django model using the app registry.
-
-        This doesn't require that an app with the given app label exists,
-        which makes it safe to call when the registry is being populated.
-        All other methods to access models might raise an exception about the
-        registry not being ready yet.
-        Raises LookupError if model isn't found.
+        Given an app label and a model name, returns the corresponding model class.
         """
         try:
             return apps.get_model(app_label, model_name)
         except AppRegistryNotReady:
             if apps.apps_ready and not apps.models_ready:
-                # If this function is called while `apps.populate()` is
-                # loading models, ensure that the module that defines the
-                # target model has been imported and try looking the model up
-                # in the app registry. This effectively emulates
-                # `from path.to.app.models import Model` where we use
-                # `Model = get_model('app', 'Model')` instead.
+                # If the models module of the considered app has not been loaded yet,
+                # we try to import it manually in order to retrieve the model class.
+                # This trick is usefull in order to get the model class during the
+                # ``apps.populate()`` call and the execution of related methods of AppConfig
+                # instances (eg. ``ready``).
+
+                # Firts, the config of the consideration must be retrieved
                 app_config = apps.get_app_config(app_label)
-                # `app_config.import_models()` cannot be used here because it
-                # would interfere with `apps.populate()`.
+                # Then the models module is manually imported
                 import_module('%s.%s' % (app_config.name, MODELS_MODULE_NAME))
-                # In order to account for case-insensitivity of model_name,
-                # look up the model through a private API of the app registry.
+                # Finally, tries to return the registered model class
                 return apps.get_registered_model(app_label, model_name)
             else:
-                # This must be a different case (e.g. the model really doesn't
-                # exist). We just re-raise the exception.
                 raise
 
     def is_model_registered(app_label, model_name):
