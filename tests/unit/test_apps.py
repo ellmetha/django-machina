670fc7dbe84b7a12338701e3dcca30d5ff712d37
ellmetha <morgan.aubert@zoho.com>
2015-11-21 16:50:41 +0100

    Unit tests addede

---
+++ tests/unit/test_apps.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+from __future__ import unicode_literals
+
+# Third party imports
+# Local application / specific library imports
+from machina import get_apps
+from machina import MACHINA_VANILLA_APPS
+
+
+class TestAppGetter(object):
+    def test_can_return_the_vanilla_applications(self):
+        # Run & check
+        assert get_apps() == MACHINA_VANILLA_APPS
+
+    def test_can_use_overridden_applications(self):
+        # Run & check
+        apps = get_apps(overrides=['test.forum'])
+        assert 'test.forum' in apps
+
+    def test_cannot_use_overridden_applications_with_invalid_names(self):
+        # Run & check
+        apps = get_apps(overrides=['test.forum_alt'])
+        assert 'test.forum_alt' not in apps
