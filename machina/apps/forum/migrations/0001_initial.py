6675e92b5c98c36bbb5d3a6e9117cb05e2a44d63
ellmetha <morgan.aubert@zoho.com>
2016-01-17 23:24:03 -0500

    Fixed forum migration

--- machina/apps/forum/migrations/0001_initial.py
+++ machina/apps/forum/migrations/0001_initial.py
@@ -43,7 +43,7 @@ class Migration(migrations.Migration):
                 'abstract': False,
                 'verbose_name': 'Forum',
                 'verbose_name_plural': 'Forums',
-                'permissions': [('can_see_forum', 'Can see forum'), ('can_read_forum', 'Can read forum'), ('can_start_new_topics', 'Can start new topics'), ('can_reply_to_topics', 'Can reply to topics'), ('can_post_announcements', 'Can post announcements'), ('can_post_stickies', 'Can post stickies'), ('can_delete_own_posts', 'Can delete own posts'), ('can_edit_own_posts', 'Can edit own posts'), ('can_post_without_approval', 'Can post without approval'), ('can_create_poll', 'Can create poll'), ('can_vote_in_polls', 'Can vote in polls'), ('can_attach_file', 'Can attach file'), ('can_download_file', 'Can download file'), ('can_close_topics', 'Can close topics'), ('can_move_topics', 'Can move topics'), ('can_edit_posts', 'Can edit posts'), ('can_delete_posts', 'Can delete posts'), ('can_move_posts', 'Can move posts'), ('can_approve_posts', 'Can approve posts')],
+                'permissions': [],
             },
         ),
     ]
