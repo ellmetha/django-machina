eb1f946f9682977a7380450d52993b7e70a5a5bb
Morgan Aubert <morgan.aubert@zoho.com>
2016-02-24 23:18:00 -0500

    Fix. member profile signature field max length

--- machina/apps/forum_member/abstract_models.py
+++ machina/apps/forum_member/abstract_models.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Standard library imports
 from __future__ import unicode_literals
 
-# Third party imports
 from django.conf import settings
+from django.core import validators
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
-# Local application / specific library imports
 from machina.conf import settings as machina_settings
 from machina.models.fields import ExtendedImageField
 from machina.models.fields import MarkupTextField
@@ -23,12 +21,15 @@ class AbstractForumProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_('User'), related_name='forum_profile')
 
     # The user's avatar
-    avatar = ExtendedImageField(verbose_name=_('Avatar'), null=True, blank=True,
-                                upload_to=machina_settings.PROFILE_AVATAR_UPLOAD_TO,
-                                **machina_settings.DEFAULT_AVATAR_SETTINGS)
+    avatar = ExtendedImageField(
+        verbose_name=_('Avatar'), null=True, blank=True,
+        upload_to=machina_settings.PROFILE_AVATAR_UPLOAD_TO,
+        **machina_settings.DEFAULT_AVATAR_SETTINGS)
 
     # The user's signature
-    signature = MarkupTextField(verbose_name=_('Signature'), max_length=machina_settings.PROFILE_SIGNATURE_MAX_LENGTH, blank=True, null=True)
+    signature = MarkupTextField(
+        verbose_name=_('Signature'), blank=True, null=True,
+        validators=[validators.MaxLengthValidator(machina_settings.PROFILE_SIGNATURE_MAX_LENGTH)])
 
     # The amount of posts the user has posted
     posts_count = models.PositiveIntegerField(verbose_name=_('Total posts'), blank=True, default=0)
