91f8dc8dd200da3827c7782dfd865f2d2dbe5678
Morgan Aubert <morgan.aubert@zoho.com>
2016-03-17 21:37:47 -0400

    Fix forum_attachments and forum_polls models definition

--- machina/apps/forum_conversation/forum_attachments/models.py
+++ machina/apps/forum_conversation/forum_attachments/models.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 from machina.apps.forum_conversation.forum_attachments.abstract_models import AbstractAttachment
+from machina.core.db.models import model_factory
 
 
-class Attachment(AbstractAttachment):
-    pass
+Attachment = model_factory(AbstractAttachment)
--- machina/apps/forum_conversation/forum_polls/models.py
+++ machina/apps/forum_conversation/forum_polls/models.py
@@ -5,15 +5,9 @@ from __future__ import unicode_literals
 from machina.apps.forum_conversation.forum_polls.abstract_models import AbstractTopicPoll
 from machina.apps.forum_conversation.forum_polls.abstract_models import AbstractTopicPollOption
 from machina.apps.forum_conversation.forum_polls.abstract_models import AbstractTopicPollVote
+from machina.core.db.models import model_factory
 
 
-class TopicPoll(AbstractTopicPoll):
-    pass
-
-
-class TopicPollOption(AbstractTopicPollOption):
-    pass
-
-
-class TopicPollVote(AbstractTopicPollVote):
-    pass
+TopicPoll = model_factory(AbstractTopicPoll)
+TopicPollOption = model_factory(AbstractTopicPollOption)
+TopicPollVote = model_factory(AbstractTopicPollVote)
