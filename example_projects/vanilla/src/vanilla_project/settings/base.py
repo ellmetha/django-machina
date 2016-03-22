415dbf97707c8e09a777bacead3b184367f765da
mmeinzer <meinzer.matthew@gmail.com>
2016-03-21 23:09:40 -0400

    change localization to en

--- example_projects/vanilla/src/vanilla_project/settings/base.py
+++ example_projects/vanilla/src/vanilla_project/settings/base.py
@@ -26,7 +26,7 @@ TIME_ZONE = 'Europe/Paris'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'fr'
+LANGUAGE_CODE = 'en'
 
 LANGUAGES = (
    ('en', "English"),
