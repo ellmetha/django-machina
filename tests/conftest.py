3494f5bae7c9530075729aa2de27b649c85f935e
Morgan Aubert <morgan.aubert@zoho.com>
2016-11-17 23:36:47 -0500

    Improve tests configuration

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+
+import pytest
+
+from . import settings
+
+
+@pytest.yield_fixture(scope='session', autouse=True)
+def empty_media():
+    """ Removes the directories inside the MEDIA_ROOT that could have been filled during tests. """
+    yield
+    for candidate in os.listdir(settings.MEDIA_ROOT):
+        path = os.path.join(settings.MEDIA_ROOT, candidate)
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            pass
