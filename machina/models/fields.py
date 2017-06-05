6d232d23eea53cdf305b6439ab9489c4a8fc9a91
Greg Reinbach <greg@reinbach.com>
2017-06-05 06:40:46 -0400

    Return None, instead of raising AttributeError on MarkupTextDescriptor if no instance
    Django 1.11.2 get_or_create object is passing in None instance

--- machina/models/fields.py
+++ machina/models/fields.py
@@ -109,7 +109,7 @@ class MarkupTextDescriptor(object):
 
     def __get__(self, instance, owner):
         if instance is None:
-            raise AttributeError(_('Can only be accessed via an instance.'))
+            return None
         raw = instance.__dict__[self.field.name]
         if raw is None:
             return None
