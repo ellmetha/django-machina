1f7dea13d24ae49adc7ba6fb641eac1120605335
Morgan Aubert <morgan.aubert@zoho.com>
2017-12-02 12:45:52 -0500

    Fixed #95 -- Fixed inconsistent app_label for the forum_feeds application

--- machina/apps/forum_feeds/registry_config.py
+++ machina/apps/forum_feeds/registry_config.py
@@ -7,6 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class FeedsRegistryConfig(AppConfig):
-    label = 'feeds'
+    label = 'forum_feeds'
     name = 'machina.apps.forum_feeds'
     verbose_name = _('Machina: Forum feeds')
