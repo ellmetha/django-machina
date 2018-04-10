5b1820f187991c00d7254c4fef39f4d79e4ca054
Klemen Pukl <klemen.pukl@velis.si>
2018-04-10 08:24:07 +0200

    User display

--- machina/templatetags/forum_user_tags.py
+++ machina/templatetags/forum_user_tags.py
@@ -4,9 +4,10 @@ from machina.conf.settings import FORUM_USER_DISPLAY
 
 register = template.Library()
 
+
 @register.simple_tag
 def get_username(user_id):
-    user=User.objects.get(pk=user_id)
+    user = User.objects.get(pk=user_id)
     if FORUM_USER_DISPLAY:
         user = template.Context(dict(user=user))
         t = template.Template(FORUM_USER_DISPLAY)
