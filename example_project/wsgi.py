a988ad61d32091faf8f1eac613ce34970072b24c
Morgan Aubert <morgan@aubert.email>
2019-06-22 12:32:22 -0400

    Update example project

--- example_project/wsgi.py
+++ example_project/wsgi.py
@@ -5,5 +5,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example_project.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.dev')
 application = get_wsgi_application()
