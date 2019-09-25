8c86f8ab3afcbeaf4ca0f5197312406a1fcdcb96
Brenda <brendahellenthal@gmail.com>
2019-09-25 10:03:25 +0200

    In UserForumPermissionAdmin show if authenticated user was targeted
    Add a column with boolean value to show if the authenticated user (as opposed
    to one specific user or anonymous user) is targeted for a permission.

--- machina/apps/forum_permission/admin.py
+++ machina/apps/forum_permission/admin.py
@@ -37,7 +37,8 @@ class UserForumPermissionAdmin(admin.ModelAdmin):
     """ The User Forum Permission model admin. """
 
     search_fields = ('permission__codename', 'user__username', )
-    list_display = ('user', 'anonymous_user', 'forum', 'permission', 'has_perm', )
+    list_display = ('user', 'anonymous_user', 'authenticated_user', 'forum',
+                    'permission', 'has_perm', )
     list_editables = ('has_perm', )
     raw_id_fields = ('user', )
     list_filter = ['forum']
