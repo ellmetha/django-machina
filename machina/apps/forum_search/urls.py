6e86bb52575541f88813fc10b500f11671d5a775
savelmtr <savelmtr@gmail.com>
2020-02-01 01:02:19 +0300

    Added support for postgres search

--- machina/apps/forum_search/urls.py
+++ machina/apps/forum_search/urls.py
@@ -9,7 +9,7 @@
 
 from django.urls import path
 from haystack.views import search_view_factory
-
+from machina.conf import settings
 from machina.core.loading import get_class
 from machina.core.urls import URLPatternsFactory
 
@@ -19,18 +19,22 @@ class ForumSearchURLPatternsFactory(URLPatternsFactory):
 
     app_namespace = 'forum_search'
 
-    search_view = get_class('forum_search.views', 'FacetedSearchView')
-    search_form = get_class('forum_search.forms', 'SearchForm')
+    if settings.SEARCH_ENGINE == 'haystack':
+        search_view = get_class('forum_search.views', 'FacetedSearchView')
+        search_form = get_class('forum_search.forms', 'SearchForm')
+        search_path = path(
+                '',
+                search_view_factory(view_class=search_view, form_class=search_form),
+                name='search',
+            )
+    elif settings.SEARCH_ENGINE == 'postgres':
+        search_view = get_class('forum_search.views', 'PostgresSearchView')
+        search_path = path('', search_view.as_view(), name='search')
+
 
     def get_urlpatterns(self):
         """ Returns the URL patterns managed by the considered factory / application. """
-        return [
-            path(
-                '',
-                search_view_factory(view_class=self.search_view, form_class=self.search_form),
-                name='search',
-            ),
-        ]
+        return [ self.search_path, ]
 
 
 urlpatterns_factory = ForumSearchURLPatternsFactory()
--- machina/apps/forum_search/views.py
+++ machina/apps/forum_search/views.py
@@ -6,6 +6,11 @@
 
 """
 
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.views import View
+from machina.apps.forum_search.forms import PostgresSearchForm
+from machina.conf import settings
 from haystack import views
 
 
@@ -17,3 +22,36 @@ class FacetedSearchView(views.FacetedSearchView):
     def build_form(self):
         form = super().build_form(form_kwargs={'user': self.request.user})
         return form
+
+class PostgresSearchView(View):
+    """ Allows to search using postgres search. """
+
+    form_class = PostgresSearchForm
+    template = 'forum_search/postgres_search.html'
+
+    def get(self, request, *args, **kwargs):
+
+        form = self.form_class(request)
+        
+        if 'q' in request.GET:
+            result = form.search() 
+            paginator = Paginator(result, settings.TOPIC_POSTS_NUMBER_PER_PAGE)
+            page_num = request.GET['page'] if 'page' in request.GET else 1
+            page = paginator.page(page_num)
+            context = { 
+                        'form': form,
+                        'result_count': len(result),
+                        'query': form.cleaned_data.get('q'), 
+                        'page': page, 
+                        'paginator': paginator, 
+                       }
+        else:
+            context = { 
+                        'form': form,
+                        'query': False, 
+                       } 
+        
+        return render(request, self.template, context)
+
+    def post(self, request, *args, **kwargs):
+        self.get(request, *args, **kwargs)
