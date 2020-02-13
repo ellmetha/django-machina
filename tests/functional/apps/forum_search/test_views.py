a0cf40c2b36a88caa54f5a82d689ec7f54d72087
savelmtr <savelmtr@gmail.com>
2020-02-14 00:59:00 +0300

    added tests for postgres search

--- tests/functional/apps/forum_search/test_views.py
+++ tests/functional/apps/forum_search/test_views.py
@@ -9,6 +9,7 @@ from haystack.query import SearchQuerySet
 
 from machina.core.db.models import get_model
 from machina.core.loading import get_class
+from machina.test import override_settings
 from machina.test.factories import PostFactory, create_category_forum, create_forum, create_topic
 from machina.test.testcases import BaseClientTestCase
 
@@ -95,6 +96,80 @@ class TestFacetedSearchView(BaseClientTestCase):
     @classmethod
     def teardown_class(cls):
         shutil.rmtree(settings.HAYSTACK_CONNECTIONS['default']['PATH'])
+
+    def test_can_search_forum_posts(self):
+        # Setup
+        correct_url = reverse('forum_search:search')
+        get_data = {'q': self.topic_1.subject}
+        # Run
+        response = self.client.get(correct_url, data=get_data)
+        # Check
+        assert response.status_code == 200
+        assert len(response.context['page'].object_list) == 1
+        assert response.context['page'].object_list[0].object == self.post_1
+
+
+@override_settings(MACHINA_SEARCH_ENGINE = 'postgres')
+class TestPostgresSearchView(BaseClientTestCase):
+    @pytest.yield_fixture(autouse=True)
+    def setup(self):
+        management.call_command('migrate')
+        # Permission handler
+        self.perm_handler = PermissionHandler()
+
+        # Set up the following forum tree:
+        #
+        #     top_level_cat
+        #         forum_1
+        #         forum_2
+        #             forum_2_child_1
+        #     top_level_forum_1
+        #     top_level_forum_2
+        #         sub_cat
+        #             sub_sub_forum
+        #     top_level_forum_3
+        #         forum_3
+        #             forum_3_child_1
+        #                 forum_3_child_1_1
+        #                     deep_forum
+        #     last_forum
+        #
+        self.top_level_cat = create_category_forum()
+
+        self.forum_1 = create_forum(parent=self.top_level_cat)
+        self.forum_2 = create_forum(parent=self.top_level_cat)
+        self.forum_2_child_1 = create_forum(parent=self.forum_2)
+
+        self.top_level_forum_1 = create_forum()
+
+        self.top_level_forum_2 = create_forum()
+        self.sub_cat = create_category_forum(parent=self.top_level_forum_2)
+        self.sub_sub_forum = create_forum(parent=self.sub_cat)
+
+        self.top_level_forum_3 = create_forum()
+        self.forum_3 = create_forum(parent=self.top_level_forum_3)
+        self.forum_3_child_1 = create_forum(parent=self.forum_3)
+        self.forum_3_child_1_1 = create_forum(parent=self.forum_3_child_1)
+        self.deep_forum = create_forum(parent=self.forum_3_child_1_1)
+
+        self.last_forum = create_forum()
+
+        # Set up a topic and some posts
+        self.topic_1 = create_topic(forum=self.forum_1, poster=self.user)
+        self.post_1 = PostFactory.create(topic=self.topic_1, poster=self.user)
+        self.topic_2 = create_topic(forum=self.forum_2, poster=self.user)
+        self.post_2 = PostFactory.create(topic=self.topic_2, poster=self.user)
+        self.topic_3 = create_topic(forum=self.forum_2_child_1, poster=self.user)
+        self.post_3 = PostFactory.create(topic=self.topic_3, poster=self.user)
+
+        # Assign some permissions
+        assign_perm('can_read_forum', self.user, self.top_level_cat)
+        assign_perm('can_read_forum', self.user, self.forum_1)
+        assign_perm('can_read_forum', self.user, self.forum_2)
+        assign_perm('can_read_forum', self.user, self.forum_2_child_1)
+        assign_perm('can_read_forum', self.user, self.top_level_forum_1)
+
+        yield
 
     def test_can_search_forum_posts(self):
         # Setup
--- tests/unit/apps/forum_search/test_forms.py
+++ tests/unit/apps/forum_search/test_forms.py
@@ -3,12 +3,14 @@ import shutil
 import pytest
 from django.conf import settings
 from django.core import management
+from django.http import HttpRequest
 from faker import Faker
 from haystack.query import SearchQuerySet
 
-from machina.apps.forum_search.forms import SearchForm
+from machina.apps.forum_search.forms import PostgresSearchForm, SearchForm
 from machina.core.db.models import get_model
 from machina.core.loading import get_class
+from machina.test import override_settings
 from machina.test.factories import (
     PostFactory, UserFactory, create_category_forum, create_forum, create_topic
 )
@@ -225,3 +227,182 @@ class TestSearchForm(object):
         # Check
         assert form.is_valid()
         assert [r.object for r in results] == [self.post_1, self.post_2, ]
+
+@override_settings(MACHINA_SEARCH_ENGINE = 'postgres')
+class TestPostgresSearchForm(object):
+    @pytest.yield_fixture(autouse=True)
+    def setup(self):
+        management.call_command('migrate')
+        # Permission handler
+        self.perm_handler = PermissionHandler()
+
+        # Create a basic user
+        self.user = UserFactory.create(username='foobar')
+
+        # Set up the following forum tree:
+        #
+        #     top_level_cat
+        #         forum_1
+        #         forum_2
+        #             forum_2_child_1
+        #     top_level_forum_1
+        #     top_level_forum_2
+        #         sub_cat
+        #             sub_sub_forum
+        #     top_level_forum_3
+        #         forum_3
+        #             forum_3_child_1
+        #                 forum_3_child_1_1
+        #                     deep_forum
+        #     last_forum
+        #
+        self.top_level_cat = create_category_forum()
+
+        self.forum_1 = create_forum(parent=self.top_level_cat)
+        self.forum_2 = create_forum(parent=self.top_level_cat)
+        self.forum_2_child_1 = create_forum(parent=self.forum_2)
+
+        self.top_level_forum_1 = create_forum()
+
+        self.top_level_forum_2 = create_forum()
+        self.sub_cat = create_category_forum(parent=self.top_level_forum_2)
+        self.sub_sub_forum = create_forum(parent=self.sub_cat)
+
+        self.top_level_forum_3 = create_forum()
+        self.forum_3 = create_forum(parent=self.top_level_forum_3)
+        self.forum_3_child_1 = create_forum(parent=self.forum_3)
+        self.forum_3_child_1_1 = create_forum(parent=self.forum_3_child_1)
+        self.deep_forum = create_forum(parent=self.forum_3_child_1_1)
+
+        self.last_forum = create_forum()
+
+        # Set up a topic and some posts
+        self.topic_1 = create_topic(forum=self.forum_1, poster=self.user)
+        self.post_1 = PostFactory.create(topic=self.topic_1, poster=self.user)
+        self.topic_2 = create_topic(forum=self.forum_2, poster=self.user)
+        self.post_2 = PostFactory.create(topic=self.topic_2, poster=self.user)
+        self.topic_3 = create_topic(forum=self.forum_2_child_1, poster=self.user)
+        self.post_3 = PostFactory.create(topic=self.topic_3, poster=self.user)
+
+        # Assign some permissions
+        assign_perm('can_read_forum', self.user, self.top_level_cat)
+        assign_perm('can_read_forum', self.user, self.forum_1)
+        assign_perm('can_read_forum', self.user, self.forum_2)
+        assign_perm('can_read_forum', self.user, self.forum_2_child_1)
+        assign_perm('can_read_forum', self.user, self.top_level_forum_1)
+
+        self.request = HttpRequest
+
+        yield
+
+    def test_can_search_forum_posts(self):
+        # Setup
+        self.request.user = self.user
+        self.request.GET = {'q': self.topic_1.first_post.subject}
+        form = PostgresSearchForm(request=self.request)
+        # Run
+        results = form.search()
+        # Check
+        assert form.is_valid()
+        assert results[0].forum.pk == self.topic_1.forum.pk
+
+    def test_cannot_search_forum_posts_if_the_user_has_not_the_required_permissions(self):
+        # Setup
+        u1 = UserFactory.create()
+        self.request.user = u1
+        self.request.GET = {'q': self.topic_1.first_post.content}
+        form = PostgresSearchForm(request=self.request)
+        # Run
+        results = form.search()
+        # Check
+        assert form.is_valid()
+        assert not len(results)
+
+    def test_cannot_search_forum_posts_if_the_form_is_not_valid(self):
+        # Setup
+        self.request.user = self.user
+        self.request.GET = {
+            'q': self.topic_1.first_post.content,
+            'search_forums': [1000, ],
+        }
+        form = PostgresSearchForm(request=self.request)
+        # Run
+        results = form.search()
+        # Check
+        assert not len(results)
+
+    def test_can_search_forum_posts_by_using_only_topic_subjects(self):
+        # Setup
+        self.request.user = self.user
+        self.request.GET = {
+            'q': self.topic_1.subject,
+            'search_topics': True,
+        }
+        form = PostgresSearchForm(request=self.request)
+        # Run
+        results = form.search()
+        # Check
+        assert form.is_valid()
+        assert results[0].forum.pk == self.topic_1.forum.pk
+
+    def test_can_search_forum_posts_by_using_the_registered_poster_name(self):
+        # Setup
+        self.topic_1.first_post.subject = 'newsubject'
+        self.topic_1.first_post.save()
+        self.topic_2.first_post.subject = 'newsubject'
+        self.topic_2.first_post.save()
+        self.topic_3.first_post.subject = 'newsubject'
+        self.topic_3.first_post.save()
+
+        self.request.user = self.user
+        self.request.GET = {
+            'q': 'newsubject',
+            'search_poster_name': self.user.username,
+        }
+        form = PostgresSearchForm(request=self.request)
+        # Run
+        results = form.search()
+        # Check
+        assert form.is_valid()
+        assert results == [self.post_1, self.post_2, self.post_3, ]
+
+    def test_can_search_forum_posts_by_using_the_anonymous_poster_name(self):
+        # Setup
+        self.topic_1.first_post.subject = 'newsubject'
+        self.topic_1.first_post.save()
+        self.topic_2.first_post.subject = 'newsubject'
+        self.topic_2.first_post.save()
+        self.topic_3.first_post.subject = 'newsubject'
+        self.topic_3.first_post.save()
+        post_4 = PostFactory.create(
+            subject='newsubject', topic=self.topic_3, poster=None, username='newtest')
+
+        self.request.user = self.user
+        self.request.GET = {
+            'q': 'newsubject',
+            'search_poster_name': 'newtest',
+       }
+        form = PostgresSearchForm(request=self.request)
+        # Run
+        results = form.search()
+        # Check
+        assert form.is_valid()
+        assert results == [post_4, ]
+
+    def test_can_search_forum_posts_by_using_a_set_of_forums(self):
+        # Setup
+        self.topic_2.first_post.subject = self.topic_1.subject
+        self.topic_2.first_post.save()
+
+        self.request.user = self.user
+        self.request.GET = {
+            'q': self.topic_1.subject,
+            'search_forums': [self.forum_1.pk, self.forum_2.pk, ],
+        }
+
+        form = PostgresSearchForm(request=self.request)
+        # Run
+        results = form.search()
+        # Check
+        assert form.is_valid()
+        assert results == [self.post_1, self.post_2, ]
