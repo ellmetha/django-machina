afb1dd795ff78f9990d5a5898c9c1b78f24798e6
savelmtr <savelmtr@gmail.com>
2020-04-26 14:17:57 +0300

    making some tests...

--- tests/unit/apps/forum_search/test_forms.py
+++ tests/unit/apps/forum_search/test_forms.py
@@ -1,4 +1,3 @@
-'''
 import shutil
 
 import pytest
@@ -7,9 +6,8 @@ from django.core import management
 from django.http import HttpRequest
 from django.test import override_settings
 from faker import Faker
-from haystack.query import SearchQuerySet
 
-from machina.apps.forum_search.forms import PostgresSearchForm, SearchForm
+from machina.apps.forum_search.forms import PostgresSearchForm
 from machina.core.db.models import get_model
 from machina.core.loading import get_class
 from machina.test.factories import (
@@ -28,8 +26,80 @@ assign_perm = get_class('forum_permission.shortcuts', 'assign_perm')
 
 @pytest.mark.django_db
 class TestPostgresSearchForm(object):
-    pass
+    @pytest.yield_fixture(autouse=True)
+    def setup(self):
+        # Permission handler
+        self.perm_handler = PermissionHandler()
 
+        # Create a basic user
+        self.user = UserFactory.create(username='foobar')
+
+        # Set up the following forum tree:
+        #
+        #     top_level_cat
+        #         forum_1
+        #         forum_2
+        #             forum_2_child_1
+        #     top_level_forum_1
+        #     top_level_forum_2
+        #         sub_cat
+        #             sub_sub_forum
+        #     top_level_forum_3
+        #         forum_3
+        #             forum_3_child_1
+        #                 forum_3_child_1_1
+        #                     deep_forum
+        #     last_forum
+        #
+        self.top_level_cat = create_category_forum()
+
+        self.forum_1 = create_forum(parent=self.top_level_cat)
+        self.forum_2 = create_forum(parent=self.top_level_cat)
+        self.forum_2_child_1 = create_forum(parent=self.forum_2)
+
+        self.top_level_forum_1 = create_forum()
+
+        self.top_level_forum_2 = create_forum()
+        self.sub_cat = create_category_forum(parent=self.top_level_forum_2)
+        self.sub_sub_forum = create_forum(parent=self.sub_cat)
+
+        self.top_level_forum_3 = create_forum()
+        self.forum_3 = create_forum(parent=self.top_level_forum_3)
+        self.forum_3_child_1 = create_forum(parent=self.forum_3)
+        self.forum_3_child_1_1 = create_forum(parent=self.forum_3_child_1)
+        self.deep_forum = create_forum(parent=self.forum_3_child_1_1)
+
+        self.last_forum = create_forum()
+
+        # Set up a topic and some posts
+        self.topic_1 = create_topic(forum=self.forum_1, poster=self.user)
+        self.post_1 = PostFactory.create(topic=self.topic_1, poster=self.user)
+        self.topic_2 = create_topic(forum=self.forum_2, poster=self.user)
+        self.post_2 = PostFactory.create(topic=self.topic_2, poster=self.user)
+        self.topic_3 = create_topic(forum=self.forum_2_child_1, poster=self.user)
+        self.post_3 = PostFactory.create(topic=self.topic_3, poster=self.user)
+
+        # Assign some permissions
+        assign_perm('can_read_forum', self.user, self.top_level_cat)
+        assign_perm('can_read_forum', self.user, self.forum_1)
+        assign_perm('can_read_forum', self.user, self.forum_2)
+        assign_perm('can_read_forum', self.user, self.forum_2_child_1)
+        assign_perm('can_read_forum', self.user, self.top_level_forum_1)
+
+        yield
+
+    def test_can_search_forum_posts(self):
+        # Setup
+        form = PostgresSearchForm(
+            {'q': self.topic_1.first_post.subject},
+            user=self.user,
+        )
+        # Run
+        results = form.search()
+        # Check
+        assert form.is_valid()
+        assert results[0].topic.forum == self.topic_1.forum.pk
+'''
 @pytest.mark.django_db
 class TestSearchForm(object):
     @pytest.yield_fixture(autouse=True)
