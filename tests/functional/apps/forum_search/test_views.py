1dfb89f3eff372bd200c74fae75843d3e031fe5f
savelmtr <savelmtr@gmail.com>
2020-04-26 23:42:11 +0300

    tests failing17

--- tests/functional/apps/forum_search/test_views.py
+++ tests/functional/apps/forum_search/test_views.py
@@ -1,4 +1,3 @@
-'''
 import shutil
 
 import pytest
@@ -7,7 +6,6 @@ from django.core import management
 from django.test import override_settings
 from django.urls import reverse
 from faker import Faker
-from haystack.query import SearchQuerySet
 
 from machina.core.db.models import get_model
 from machina.core.loading import get_class
@@ -24,6 +22,69 @@ PermissionHandler = get_class('forum_permission.handler', 'PermissionHandler')
 assign_perm = get_class('forum_permission.shortcuts', 'assign_perm')
 
 
+class TestPostgresSearchView(BaseClientTestCase):
+    @pytest.yield_fixture(autouse=True)
+    def setup(self):
+        # Permission handler
+        self.perm_handler = PermissionHandler()
+
+        # Set up the following forum tree:
+        #
+        #     top_level_cat
+        #         forum_1
+        #         forum_2
+        #             forum_2_child_1
+        #     top_level_forum_1
+        #     top_level_forum_2
+        #         sub_cat
+        #             sub_sub_forum
+        #     top_level_forum_3
+        #         forum_3
+        #             forum_3_child_1
+        #                 forum_3_child_1_1
+        #                     deep_forum
+        #     last_forum
+        #
+        self.top_level_cat = create_category_forum()
+
+        self.forum_1 = create_forum(parent=self.top_level_cat)
+        self.forum_2 = create_forum(parent=self.top_level_cat)
+        self.forum_2_child_1 = create_forum(parent=self.forum_2)
+
+        self.top_level_forum_1 = create_forum()
+
+        self.top_level_forum_2 = create_forum()
+        self.sub_cat = create_category_forum(parent=self.top_level_forum_2)
+        self.sub_sub_forum = create_forum(parent=self.sub_cat)
+
+        self.top_level_forum_3 = create_forum()
+        self.forum_3 = create_forum(parent=self.top_level_forum_3)
+        self.forum_3_child_1 = create_forum(parent=self.forum_3)
+        self.forum_3_child_1_1 = create_forum(parent=self.forum_3_child_1)
+        self.deep_forum = create_forum(parent=self.forum_3_child_1_1)
+
+        self.last_forum = create_forum()
+
+        # Set up a topic and some posts
+        self.topic_1 = create_topic(forum=self.forum_1, poster=self.user)
+        self.post_1 = PostFactory.create(topic=self.topic_1, poster=self.user)
+        self.topic_2 = create_topic(forum=self.forum_2, poster=self.user)
+        self.post_2 = PostFactory.create(topic=self.topic_2, poster=self.user)
+        self.topic_3 = create_topic(forum=self.forum_2_child_1, poster=self.user)
+        self.post_3 = PostFactory.create(topic=self.topic_3, poster=self.user)
+
+        # Assign some permissions
+        assign_perm('can_read_forum', self.user, self.top_level_cat)
+        assign_perm('can_read_forum', self.user, self.forum_1)
+        assign_perm('can_read_forum', self.user, self.forum_2)
+        assign_perm('can_read_forum', self.user, self.forum_2_child_1)
+        assign_perm('can_read_forum', self.user, self.top_level_forum_1)
+
+
+        yield
+
+
+'''
 class TestFacetedSearchView(BaseClientTestCase):
     @pytest.yield_fixture(autouse=True)
     def setup(self):
