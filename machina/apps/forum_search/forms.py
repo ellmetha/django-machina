594fcdc5f4447998aa5b54e3a7a4cadcadb64b72
savelmtr <savelmtr@gmail.com>
2020-05-09 13:47:13 +0300

    ugettext_lazy changed to gettext_lazy in forms.py

--- machina/apps/forum_search/forms.py
+++ machina/apps/forum_search/forms.py
@@ -8,7 +8,7 @@
 
 from django import forms
 from django.db.models import Q
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from machina.conf import settings
 from machina.core.db.models import get_model
