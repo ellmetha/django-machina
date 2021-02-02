a53524de7a740ddbb0d4c1b7602eb595b05057b8
carofun <caroline.cramoisan@fun-mooc.fr>
2021-02-02 15:45:06 +0100

    Remove deprecated providing_args argument
    RemovedInDjango40Warning: The providing_args argument is deprecated.
    As it is purely documentational, it has no replacement. If you rely
    on this argument as documentation, you can move the text to a code
    comment or docstring.

--- machina/apps/forum/signals.py
+++ machina/apps/forum/signals.py
@@ -8,6 +8,7 @@
 
 import django.dispatch
 
-
-forum_moved = django.dispatch.Signal(providing_args=["previous_parent", ])
-forum_viewed = django.dispatch.Signal(providing_args=["forum", "user", "request", "response", ])
+# Arguments: "previous_parent"
+forum_moved = django.dispatch.Signal()
+# Arguments: "forum", "user", "request", "response"
+forum_viewed = django.dispatch.Signal()
