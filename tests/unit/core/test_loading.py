7c5f06754ad286b658c56bb1beb86ad2342e9ea1
Bo <BoPeng@users.noreply.github.com>
2023-01-16 18:22:08 -0700

    Allow get_class from apps.AppConfig (#288)
    * Allow get_class from apps.AppConfig
    
    * Add a test
    
    * shorten lines

--- tests/unit/core/test_loading.py
+++ tests/unit/core/test_loading.py
@@ -28,6 +28,13 @@ class TestClassLoadingFunctions(object):
         with pytest.raises(ClassNotFoundError):
             get_class('forum.models', 'Foo')
 
+    def test_get_class_with_app_config(self):
+        apps = list(settings.INSTALLED_APPS)
+        idx = apps.index('tests._testsite.apps.forum_conversation')
+        apps[idx] += '.apps.ForumConversationAppConfig'
+        with override_settings(INSTALLED_APPS=apps):
+            get_class('forum_conversation.models', 'Post')
+
     def test_raise_importerror_if_app_raises_importerror(self):
         # Setup
         apps = list(settings.INSTALLED_APPS)
