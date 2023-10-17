347a5dc215ce76e037effac3a6d28e1bacd7a32c
Morgan Aubert <morgan@aubert.email>
2023-10-16 21:52:01 -0400

    Update doc conf

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
 import django
 django.setup()
 
-ON_RTD = os.environ.get('READTHEDOCS', None) == 'True'
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
